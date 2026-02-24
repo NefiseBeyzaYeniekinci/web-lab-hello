a92129d33a9bf141a826f058a455ae9a26328622
Nefise Beyza Yeniekinci <nefisebeyzay221@gmail.com>
2026-02-24 21:43:15 +0300

    ozellik: kisisel bilgiler, dogru baslik hiyerarsisi ve alt metinler eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,27 @@ function App() {
         
         {/* Hakkımda Bölümü [cite: 85, 113] */}
         <section id="hakkimda">
-          <h2>Hakkimda</h2>
-          {/* İçerik buraya gelecek */}
-        </section>
+  {/* Sayfadaki tek ve ana başlık [cite: 192] */}
+  <h1>Nefise Beyza Yeniekinci</h1> 
+  <h2>Hakkimda</h2>
+  
+  <figure>
+    {/* alt metni görseli tanımlar, figcaption ise açıklama ekler [cite: 253, 259] */}
+    <img src="profil-fotografin.jpg" alt="Nefise Beyza Yeniekinci'nin profil fotografi" width="200" />
+    <figcaption>Yazilim Muhendisligi Ogrencisi</figcaption>
+  </figure>
 
-        {/* Projeler Bölümü [cite: 125, 127] */}
-        <section id="projeler">
-          <h2>Projelerim</h2>
-          {/* Proje kartları buraya gelecek */}
-        </section>
-
-        {/* İletişim Bölümü [cite: 134, 135] */}
-        <section id="iletisim">
-          <h2>Iletisim</h2>
-          {/* Form buraya gelecek */}
-        </section>
+  <p>
+    Merhaba! Ben Nefise Beyza. Modern web teknolojileri ve yazilim mimarileriyle ilgileniyorum.
+  </p>
+  
+  <h3>Kullandigim Teknolojiler</h3>
+  <ul>
+    <li>React & TypeScript</li>
+    <li>Git & GitHub</li>
+    <li>HTML5 & CSS3</li>
+  </ul>
+</section>
 
       </main>
 
